80e35e2dfb4ca0d8c3480dd2d2d9c7469e34c4ff
Muhammad Nuridin <muhammad.nuridin@skorpoints.com>
2021-12-05 10:02:56 +0700

    validate login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,18 +12,33 @@ export class LoginComponent implements OnInit {
   frmLogin: FormGroup;
   submitted = false;
   user = null;
+  isError: boolean = false;
+  message: string = null;
 
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
     private apiService:ApiService
-  ) { }
+  ) {
+    this.checkUserLogin();
+  }
 
   ngOnInit(): void {
     this.frmLogin = this.formBuilder.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
     });
+  }
+
+  checkUserLogin() {
+    const userData = this.fetchUserFromCookie('user');
+    const userParse = JSON.parse(userData);
+
+    if (userParse) {
+      this.router.navigateByUrl('/dashboard');
+    } else {
+      this.router.navigateByUrl('/');
+    }
   }
 
   get formErr() { return this.frmLogin.controls; }
@@ -35,7 +50,13 @@ export class LoginComponent implements OnInit {
       return;
     }
 
-    this.getUser();
+    if (this.frmLogin.value.username === this.frmLogin.value.password) {
+      this.getUser();
+    } else {
+      this.isError = true;
+      this.message = 'Username or password invalid';
+    }
+
   }
 
   getUser() {
@@ -44,10 +65,23 @@ export class LoginComponent implements OnInit {
       if (userData !== undefined) {
         this.setCookie('user', userData);
         this.router.navigate(['/dashboard']);
+        this.isError = false;
       } else {
-        console.log('password or username invalid');
+        this.isError = true;
+        this.message = 'Username or password invalid';
       }
     });
+  }
+
+  fetchUserFromCookie(name: string) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
   }
 
   setCookie(name, value) {
